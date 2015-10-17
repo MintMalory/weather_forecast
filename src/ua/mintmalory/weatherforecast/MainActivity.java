566b68b37ba48e6fa87fc3097a3bfb047b67c609
MaloryB <lugyshal@voliacable.com>
2015-10-18 02:38:24 +0300

    Added listView

--- src/ua/mintmalory/weatherforecast/MainActivity.java
+++ src/ua/mintmalory/weatherforecast/MainActivity.java
@@ -1,16 +1,22 @@
 package ua.mintmalory.weatherforecast;
 
-import android.app.Activity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ListFragment())
+                    .commit();
+        }
 	}
 
 	@Override
